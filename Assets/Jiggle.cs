73760d1f742066f716f3a7db631455984ec5b6fe
Nick Kaman <nickykaman@gmail.com>
2019-01-26 10:57:12 -0800

    partygoers

---
+++ Assets/Jiggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jiggle : MonoBehaviour
+{
+    public Collider body;
+    public Rigidbody[] rigids;
+    public float intensity = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider[] limbs = GetComponentsInChildren<Collider>();
+        for (int i = 0; i < limbs.Length; i++) {
+            Physics.IgnoreCollision(body, limbs[i]);
+        }
+        
+        StartCoroutine(JiggleRoutine());
+    }
+
+    IEnumerator JiggleRoutine()
+    {
+        //Rigidbody[] rigids = GetComponentsInChildren<Rigidbody>();
+        //yield return new WaitForSeconds(.2f * Random.value);
+        while (true)
+        {
+            for (int i = 0; i < rigids.Length; i++)
+            {
+                rigids[i].AddForce(Random.insideUnitSphere * Random.Range(50, 20)* intensity, ForceMode.Impulse);
+            }
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+    
+}
