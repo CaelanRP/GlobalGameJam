5c16229701d2ec1729f6ba6b655d12aac1c0f626
Caelan <caelanrp>
2019-01-26 10:55:35 -0800

    basic dancer gen

---
+++ Assets/PropPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NaughtyAttributes;
+
+public class PropPoint : MonoBehaviour
+{
+    void OnDrawGizmos(){
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+    }
+}
---
+++ Assets/Scripts/PartySettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NaughtyAttributes;
+
+[CreateAssetMenu]
+public class PartySettings : ScriptableObject
+{
+    [BoxGroup("Music")]
+    public int BPM;
+    [BoxGroup("Dancing")]
+    public float danceIntensity;
+    [BoxGroup("Partygoers")]
+    public float partyGoerCountAvg, partyGoerCountStdDev, partyGoerCountMin, partyGoerCountMax;
+    [BoxGroup("Partygoers")]
+    public GameObject partyGoerPrefab;
+    
+
+    public int GeneratePartygoerCount(){
+        float f = Util.GetNormalDistFloat(partyGoerCountAvg, partyGoerCountStdDev, partyGoerCountMin, partyGoerCountMax);
+        return Mathf.RoundToInt(f);
+    }
+
+    
+}
