618c2de4afdb1323101759bf6841c9332ef69b8f
Nick Kaman <nickykaman@gmail.com>
2019-01-26 11:29:46 -0800

    variants

--- Assets/Jiggle.cs
+++ Assets/Jiggle.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class Jiggle : MonoBehaviour
 {
     public Collider body;
-    public Rigidbody[] rigids;
+    public bool jiggle;
     public float intensity = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Collider[] limbs = GetComponentsInChildren<Collider>();
-        for (int i = 0; i < limbs.Length; i++) {
-            Physics.IgnoreCollision(body, limbs[i]);
+        if (body)
+        {
+            Collider[] limbs = GetComponentsInChildren<Collider>();
+            for (int i = 0; i < limbs.Length; i++)
+            {
+                Physics.IgnoreCollision(body, limbs[i]);
+            }
         }
-        
-        StartCoroutine(JiggleRoutine());
+        if (jiggle)
+        {
+            StartCoroutine(JiggleRoutine());
+        }
     }
 
     IEnumerator JiggleRoutine()
     {
-        //Rigidbody[] rigids = GetComponentsInChildren<Rigidbody>();
+        Rigidbody[] rigids = GetComponentsInChildren<Rigidbody>();
         //yield return new WaitForSeconds(.2f * Random.value);
         while (true)
         {
