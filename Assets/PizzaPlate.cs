fa87bd9b5c8dddc6b15170d2c2476a3251811d65
Caelan <caelanrp>
2019-01-26 18:20:18 -0800

    fixed things, added rare parties

--- Assets/PizzaPlate.cs
+++ Assets/PizzaPlate.cs
@@ -10,12 +10,15 @@ using NaughtyAttributes;
 public class PizzaPlate : MonoBehaviour
 {
     public List<GameObject> slices;
+    public bool removeSlices = true;
 
     void Awake(){
-        int numSlices = UnityEngine.Random.Range(1,slices.Count);
-        List<GameObject> slicesToRemove = Util.RandomSelections(slices, slices.Count - numSlices);
-        foreach(GameObject obj in slicesToRemove){
-            Destroy(obj);
+        if (removeSlices){
+            int numSlices = UnityEngine.Random.Range(1,slices.Count);
+            List<GameObject> slicesToRemove = Util.RandomSelections(slices, slices.Count - numSlices);
+            foreach(GameObject obj in slicesToRemove){
+                Destroy(obj);
+            }
         }
     }
 }
